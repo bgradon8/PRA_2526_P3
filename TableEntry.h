a9da36b1e51106c3119e2736570b230395de5638
BGRADON <bgradon@alumno.upv.es@ldsic-vdi12.upvnet.upv.es>
2026-01-05 19:33:05 +0100

    Añadidos operadores < y > a TableEntry

--- TableEntry.h
+++ TableEntry.h
@@ -10,18 +10,34 @@ public:
     std::string key;
     V value;
 
+    
     TableEntry(std::string key, V value) : key(std::move(key)), value(value) {}
+
+    
     explicit TableEntry(std::string key) : key(std::move(key)), value(V()) {}
+
+    
     TableEntry() : key(""), value(V()) {}
 
-    friend bool operator==(const TableEntry<V>& te1, const TableEntry<V>& te2) {
-        return te1.key == te2.key;
+    
+    friend bool operator==(const TableEntry<V>& a, const TableEntry<V>& b) {
+        return a.key == b.key;
     }
 
-    friend bool operator!=(const TableEntry<V>& te1, const TableEntry<V>& te2) {
-        return !(te1 == te2);
+    friend bool operator!=(const TableEntry<V>& a, const TableEntry<V>& b) {
+        return !(a == b);
     }
 
+    
+    friend bool operator<(const TableEntry<V>& a, const TableEntry<V>& b) {
+        return a.key < b.key;
+    }
+
+    friend bool operator>(const TableEntry<V>& a, const TableEntry<V>& b) {
+        return a.key > b.key;
+    }
+
+    
     friend std::ostream& operator<<(std::ostream& out, const TableEntry<V>& te) {
         out << "{" << te.key << " : " << te.value << "}";
         return out;
